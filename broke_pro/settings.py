ab377ca4f190ecba8cb175108d221adf8a41ee76
iamdavisyn <charlesdavison3766@gmail.com>
2026-04-25 23:55:52 +0100

    fix static files

--- broke_pro/settings.py
+++ broke_pro/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'broke_pro.urls'
@@ -119,6 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/' 
 
 MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
